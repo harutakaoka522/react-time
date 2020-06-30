71793baf02b6821d2d1f0f8e1dca9ab1c2346c9f
hogehoge <AAAgmail.com>
2020-06-30 15:57:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { tsPropertySignature } from '@babel/types';
 
 function App() {
+  let items = [
+    {"name": "イチゴ", "price":"100"},
+    {"name": "メロン", "price":"150"},
+    {"name": "ばなな", "price":"200"},
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <table className="table table-striped">
+        <tbody>
+        <Clock />
+        </tbody>
+        </table>
+
     </div>
-  );
+  )
+}
+
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.now = new Date();
+    this.state = {
+      time: `${this.now.getHours()} : ${this.now.getMinutes()} : ${this.now.getSeconds()}`
+    }
+
+    this.refresh = this.refresh.bind(this);
+
+  }
+
+
+  refresh(){
+    this.now = new Date();
+    this.setState((state) => ({
+      time: `${this.now.getHours()} : ${this.now.getMinutes()} : ${this.now.getSeconds()}`
+  }));
+  }
+
+  render() {
+    return <p onClick={this.refresh}>{this.state.time}</p>
+  }
 }
 
 export default App;
